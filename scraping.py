b6e003b0009cbc3a452918fc540e2a032c0da2a0
fjfernandez93 <paco.fernandez.muelas@gmail.com>
2016-09-25 11:31:07 +0200

    scrapping #1

---
+++ scraping.py
@@ -0,0 +1,18 @@
+from contextlib import closing
+from selenium.webdriver import Chrome # pip install selenium
+from selenium.webdriver.support.ui import WebDriverWait
+from bs4 import BeautifulSoup
+
+
+
+
+with closing(Chrome()) as browser:
+    url = "http://www.livescore.com/soccer/spain/primera-division/#/round-4"
+    browser.get(url)
+    # wait for the page to load
+    WebDriverWait(browser, timeout=20)
+    page_source = browser.page_source
+    nuevo = page_source.encode('UTF-8')
+    print nuevo
+    html = open("livescore.html", "r")
+    soup = BeautifulSoup(html, "html.parser")
